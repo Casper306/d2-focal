b69536511b1f69062993a69c98cfd9581e206597
casper306 <perfect306@gmail.com>
2021-07-21 02:48:17 +0000

    focal updates

---
+++ dice-roller.js
@@ -0,0 +1,24 @@
+/*const args = process.argv.slice(2);
+let num = Number(args[0]);
+function rollDice() {
+  
+  return Math.ceil(Math.random()*6);
+}
+//console.log(args);
+//console.log(process.argv);
+//console.log(typeof args);
+
+for(let i = 0; i < num; i++) {
+  
+  console.log(rollDice());
+}*/
+
+//console.log(rollDice())
+
+//console.log(num);
+//console.log(typeof num);
+
+var rank = "Imperator";
+var name = "Furiosa";
+
+console.log(rank, name);
